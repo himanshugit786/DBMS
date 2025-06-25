09b9f7d0f90c8c0c99207df38bb686caa8d8d871
himanshugit786 <himanshusharnagat678@gmail.com>
2025-06-25 11:20:17 +0530

    Add files via upload

---
+++ author.sql
@@ -0,0 +1,15 @@
+
+
+CREATE TABLE himanshudb.poets(
+    AuthorID INT PRIMARY KEY,
+    Name VARCHAR(100),
+    BirthYear INT
+);
+
+insert into himanshudb.poets
+(AuthorID, Name, birthyear)
+values
+(1,"agastya", 2005),
+(2," aman", 2006);
+
+
